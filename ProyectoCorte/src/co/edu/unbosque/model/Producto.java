0b11b9ef7d9f299c9ed1bea048d46a15f7bb123a
DynaMightt327 <malejacarvajalnepta@gmail.com>
2025-09-06 15:25:12 -0500

    Categorias

--- ProyectoCorte/src/co/edu/unbosque/model/Producto.java
+++ ProyectoCorte/src/co/edu/unbosque/model/Producto.java
@@ -1,5 +1,21 @@
 package co.edu.unbosque.model;
 
 public class Producto {
+	//Esta es una opcion, no sé si realmente las dejamos así
+	public enum Categoria{
+		MODA,
+		HOGAR_Y_MUEBLES,
+		ELECTRONICA,
+		DEPORTES,
+		ELECTRODOMESTICOS,
+		ARTE,
+		BELLEZA_Y_CUIDADO_PERSONAL,
+		HERRAMIENTAS,
+		CONSTRUCCION,
+		MASCOTAS
+		
+	}
+	
+	
 
 }
